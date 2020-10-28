13254571e6ed456033fd786b82498646e9c00946
Nick Hennessy <njhnny@gmail.com>
2020-10-27 19:52:34 -0700

    fix bugs

--- js/scripts.js
+++ js/scripts.js
@@ -5,11 +5,9 @@ $(document).ready(function(event) {
     let limit = $("input#submission").val();
     let booleans = [];
     let primeNumbers = [];
-
     for (let i = 1; i < limit; i+=1) {
       booleans.push(true);
     }
-    
     for (let i = 2; i < limit; i+=1) {
       if (booleans[i-2]) {
         for (let j = i*2; j<= limit; j += i) {
@@ -17,13 +15,11 @@ $(document).ready(function(event) {
         }
       }
     }
-  
     for (let k = 0; k < booleans.length; k+=1) {
       if (booleans[k]) {
         primeNumbers.push(k+2)
       }
-      
     }
-    $("p").text(primeNumbers);
+    $("p").text(primeNumbers.join(", "));
   });
  }); 
