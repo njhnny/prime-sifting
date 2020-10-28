90cfe18f169349a12db7c46cea5590d643efffa0
Nick Hennessy <njhnny@gmail.com>
2020-10-27 18:41:35 -0700

    create array to limit with js

--- js/scripts.js
+++ js/scripts.js
@@ -2,18 +2,16 @@ $(document).ready(function(event) {
   $("form#form").submit(function(event) {
     event.preventDefault();
     $("#output").show();
-    let wordResponse = $("input#submission").val();
-    let wordInput = wordResponse.toLowerCase();
-    
-    //wordInput.toLowerCase();
-    let wordString = wordInput.split("");
-    let reverseArray = wordString.reverse();
-    let reverseWord = reverseArray.join("");
-    if (reverseWord === wordInput) {
-      result = ("Yes! " + wordResponse + " is a palindrome!");
-    } else {
-       result = ("No, " + wordResponse + " is not a palindrome.")
+    let limit = $("input#submission").val();
+    let booleans = [];
+    let primeNumbers = [];
+
+    for (let i = 1; i < limit; i+=1) {
+      booleans.push(true);
     }
-    $("p").text(result);
+    alert(booleans);
+  
+    
+    //$("p").text(result);
   });
  }); 
